2d5c623625556d7dd7a87b163e0c57404655489b
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-06-22 09:35:41 -0300

    "Adjusting initial files"

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// Hamburger Menu Selections
+
+// Scroll-to-Top Selection
+
+//Hamburger Menu Functionality
+
+// Theme Switcher/Toggler selection 
+
+// Close Menu on Nav Menu Clicks
+
+// Scroll to Top
+
+// Theme Switcher/Toggler Functionality
