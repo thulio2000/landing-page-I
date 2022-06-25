ef70247171a4c650a82c889ed53037c39396f3cc
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-06-25 16:31:50 -0300

    "Email subscription, social icons, scroll,toggler"

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,15 @@
 // Hamburger Menu Selections
 
 // Scroll-to-Top Selection
+const scrollUp = document.querySelector("#scroll-up")
+
+scrollUp.addEventListener("click", () => {
+    window.scrollTo({
+        top:0,
+        left: 0,
+        behavior: "smooth",
+    })
+})
 
 //Hamburger Menu Functionality
 
@@ -11,3 +20,10 @@
 // Scroll to Top
 
 // Theme Switcher/Toggler Functionality
+
+const checkbox = document.querySelector("#checkbox");
+
+checkbox.addEventListener("change", () => {
+    //toggle website theme
+    document.body.classList.toggle("dark");
+})
